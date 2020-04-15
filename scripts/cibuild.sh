3d5fdf29dad767d9b30951685aefb88131d65052
Cristóbal Carnero Liñán <ccarnerolinan@gmail.com>
2020-04-15 18:20:18 +0200

    Add script for CI

---
+++ scripts/cibuild.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd "$(dirname "$0")/.."
+
+if [ -n "$1" ]; then
+    if [ "$1" = "build" ]; then
+        echo "==> Building…"
+        hugo
+    elif [ "$1" = "publish" ]; then
+        echo "==> Publishing…"
+        cd public
+        git config user.email "ccarnerolinan@gmail.com"
+        git config user.name "Cristóbal Carnero Liñán"
+        git remote add gh-page https://cristobalcl:$API_TOKEN_GITHUB@github.com/cristobalcl/cristobalcl.github.io.git
+        git add .
+        git commit -m "Publish"
+        git branch tmp-branch
+        git checkout master
+        git merge tmp-branch
+        git push gh-page master
+    fi
+else
+    echo "Nothing to do!"
+fi
